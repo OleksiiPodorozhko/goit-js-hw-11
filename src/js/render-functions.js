0a964da69c1fd33d38fe410c9b3bbf047459e9da
oleksiipodorozhko <oleksii.podorozhko@gmail.com>
2025-09-27 12:44:23 +0300

    Prepare project for starting HW

---
+++ src/js/render-functions.js
@@ -0,0 +1,26 @@
+import simpleLightbox from 'simplelightbox/dist/simple-lightbox.js';
+import simpleLightbox from 'simplelightbox/dist/simple-lightbox.min.css';
+
+// повинна приймати масив images, створювати HTML-розмітку для галереї, додавати її в контейнер галереї та викликати
+// метод екземпляра SimpleLightbox refresh(). Нічого не повертає.
+export function createGallery(images) {
+
+}
+
+//Ця функція нічого не приймає та повинна очищати вміст контейнера галереї.
+// Нічого не повертає.
+export function clearGallery(images) {
+
+}
+
+//Ця функція нічого не приймає, повинна додавати клас для відображення лоадера.
+// Нічого не повертає.
+export function showLoader(images) {
+
+}
+
+//Ця функція нічого не приймає, повинна прибирати клас для відображення лоадера.
+// Нічого не повертає.
+export function hideLoader(images) {
+
+}
