da66f50bed4b44ec6f8fad18479ae56dc2d746be
oleksiipodorozhko <oleksii.podorozhko@gmail.com>
2025-09-27 13:48:24 +0300

    GET works

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,38 @@
 import axios from 'axios';
+import { showError } from './iziToastHelper.js';
 
 //Ця функція повинна приймати один параметр query (пошукове слово, яке є рядком),
 // здійснювати HTTP-запит і повертати значення властивості data з отриманої відповіді.
-export function getImagesByQuery(query) {
+const key = '52494269-f940d25beafa0bafe61525357';
+const url = 'https://pixabay.com/api/';
 
+export function getImagesByQuery(query) {
+  axios.get(url, getParams(query))
+    .then(response => {
+      const images = response.data.hits;
+
+      if (!images || !images.length) {
+        throw new Error(
+          'Sorry, there are no images matching your search query. Please try again!'
+        );
+      }
+
+      console.log(images);
+      return images;
+    })
+    .catch(error => {
+      showError(error.message);
+    });
+}
+
+function getParams(query) {
+  return {
+    params: {
+      key: key,
+      q: query,
+      image_type: 'image/png',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  };
 }
--- src/main.js
+++ src/main.js
@@ -5,3 +5,18 @@
 // усі перевірки на довжину масиву в отриманій відповіді робимо саме в цьому файлі.
 // Імпортуй в нього функції із файлів pixabay-api.js та render-functions.js та викликай
 // їх у відповідний момент.
+
+import { getImagesByQuery } from './js/pixabay-api.js';
+
+const refs = {
+  form: document.querySelector('.form'),
+}
+
+refs.form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const inputValue = e.target.elements['search-text'].value.trim();
+
+  if(!inputValue) return;
+
+  getImagesByQuery(inputValue);
+});
